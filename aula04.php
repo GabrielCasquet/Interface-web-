104932fe4ce9e8a8833b0dfe2e1865ee9d44afbc
Aluno <gabrielcasquet1001@gmail.com>
2020-03-16 09:57:55 -0300

    Criação botões

---
+++ aula04.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>AULA 04-PHP</title>
+<link rel="stylesheet" href="aula04.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Bellota&display=swap" rel="stylesheet">
+  </head>
+  <body>
+<div class="site">
+
+  <div class="topo">
+MAJO DEMAIS
+<div class="menu">
+  <ul>
+    <li><a href="#">Página inicial</a></li>
+    <li><a href="#">Cadastro</a></li>
+    <li><a href="#">Contato</a></li>
+    <li><a href="#">Ajuda</a></li>
+    <li><a href="#">Login</a></li>
+  </ul>
+</div>
+
+
+<div class="conteudo">
+
+
+<h2>Criação de botões</h2>
+
+<a href="#" class="botao botao-azul">botão azul</a>
+<a href="#" class="botao botao-vermelho" >botão vermelho</a>
+<a href="#" class="botao botao-verde" >botão verde</a>
+</div>
+
+  <p class = "">Desenvolvido por Gabriel C. Casquet</p>
+
+</div>
+
+</div>
+
+
+
+  </div>
+
+</div>
+
+  </body>
+</html>
